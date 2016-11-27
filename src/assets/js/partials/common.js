669958203433d23c77fb4aecd6a31a4f6cdb7bc7
Ilya Purouski <MiKrob09@gmail.com>
2016-11-27 08:39:01 +0300

    kits 992

--- src/assets/js/partials/common.js
+++ src/assets/js/partials/common.js
@@ -146,12 +146,12 @@ $(document).ready(function () {
             setTimeout(function() {
                 $("#kits--imgLink-right-2").css("margin-right", "0");
             }, 1000);
-        } else {
+        }/* else { // Если не один раз анимация, то необходимо вернуть значения...
             $("#kits--imgLink-left-1").css("margin-left", "251px");
             $("#kits--imgLink-left-2").css("margin-left", "400px");
             $("#kits--imgLink-right-1").css("margin-right", "235px");
             $("#kits--imgLink-right-2").css("margin-right", "365px");
-        }
+        }*/
     });
 
     /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
