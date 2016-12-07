230516eb290b9b0cf0cff5581253de039302ba8d
Ilya Purouski <MiKrob09@gmail.com>
2016-12-07 23:55:06 +0300

    Отредактирован скрипт изменения углов фона навигации главной при ресайзе. Изменён класс к которому обращаемся для получения объекта DOM jq.

--- src/project.blocks/common.blocks/menu-main/menu-main.js
+++ src/project.blocks/common.blocks/menu-main/menu-main.js
@@ -1,56 +1,56 @@
 "use strict";
 
 /// NAV TOP responsive nebu bg && margins
-/// p.s. вынести всё в класыы и скриптом только присваивание классов сделать!.
+/// p.s. вынести всё в класы и скриптом только присваивание классов сделать!.
 function menuMainResponsive() {
     if(+$(document).scrollTop() > 50) {
         if (getPageSize()[2] > 991) {
-            $("#nav-main").css("background-size", "280% 219px");
-            $("#nav-main").css("height", "202px");
-            $("#nav-main").css("margin-top", "-110px");
-            $("#nav-main").css("padding-top", "104px");
-            $("#nav-main .logo").html('<img src="assets/img/nav/logo_mini.png" alt="">');
+            $("#menu-main").css("background-size", "280% 219px");
+            $("#menu-main").css("height", "202px");
+            $("#menu-main").css("margin-top", "-110px");
+            $("#menu-main").css("padding-top", "104px");
+            $("#menu-main .logo").html('<img src="assets/img/nav/logo_mini.png" alt="">');
         }
 
         if (getPageSize()[2] < 992) {
-            $("#nav-main").css("background-size", "300% 219px");
-            $("#nav-main").css("height", "219px");
-            $("#nav-main").css("margin-top", "-117px");
-            $("#nav-main").css("padding-top", "117px");
-            $("#nav-main .logo").html('<img src="assets/img/nav/logo.png" alt="">');
+            $("#menu-main").css("background-size", "300% 219px");
+            $("#menu-main").css("height", "219px");
+            $("#menu-main").css("margin-top", "-117px");
+            $("#menu-main").css("padding-top", "117px");
+            $("#menu-main .logo").html('<img src="assets/img/nav/logo.png" alt="">');
         }
 
         if (getPageSize()[2] < 768) {
-            $("#nav-main").css("background-size", "500% 219px");
+            $("#menu-main").css("background-size", "500% 219px");
         }
 
         if (getPageSize()[2] < 555) {
-            $("#nav-main").css("background-size", "800% 219px");
+            $("#menu-main").css("background-size", "800% 219px");
         }
     }
     else {
         if (getPageSize()[2] > 991) {
-            $("#nav-main").css("background-size", "180% 219px");
-            $("#nav-main").css("height", "219px");
-            $("#nav-main").css("margin-top", "-100px");
-            $("#nav-main").css("padding-top", "100px");
-            $("#nav-main .logo").html('<img src="assets/img/nav/logo.png" alt="">');
+            $("#menu-main").css("background-size", "180% 219px");
+            $("#menu-main").css("height", "219px");
+            $("#menu-main").css("margin-top", "-100px");
+            $("#menu-main").css("padding-top", "100px");
+            $("#menu-main .logo").html('<img src="assets/img/nav/logo.png" alt="">');
         }
 
         if (getPageSize()[2] < 992) {
-            $("#nav-main").css("background-size", "300% 219px");
-            $("#nav-main").css("height", "219px");
-            $("#nav-main").css("margin-top", "-117px");
-            $("#nav-main").css("padding-top", "117px");
-            $("#nav-main .logo").html('<img src="assets/img/nav/logo.png" alt="">');
+            $("#menu-main").css("background-size", "300% 219px");
+            $("#menu-main").css("height", "219px");
+            $("#menu-main").css("margin-top", "-117px");
+            $("#menu-main").css("padding-top", "117px");
+            $("#menu-main .logo").html('<img src="assets/img/nav/logo.png" alt="">');
         }
 
         if (getPageSize()[2] < 768) {
-            $("#nav-main").css("background-size", "500% 219px");
+            $("#menu-main").css("background-size", "500% 219px");
         }
 
         if (getPageSize()[2] < 555) {
-            $("#nav-main").css("background-size", "800% 219px");
+            $("#menu-main").css("background-size", "800% 219px");
         }
     }
 }
