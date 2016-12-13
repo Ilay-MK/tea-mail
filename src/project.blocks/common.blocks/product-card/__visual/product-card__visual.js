7caf24db2b9c6714f14ad8758ef858841af5c87b
Ilya Purouski <MiKrob09@gmail.com>
2016-12-13 13:20:33 +0300

    Исправлен скрипт добавления классов при hover карточки товара common.blocks проектного уровня

--- src/project.blocks/common.blocks/product-card/__visual/product-card__visual.js
+++ src/project.blocks/common.blocks/product-card/__visual/product-card__visual.js
@@ -2,11 +2,11 @@
 
 /// Добавление класса c модификатором hover при hover карточки продукта.
 $(function() {
-    $.extend(true, $('.product-card').hover(),
+    $.extend(true, $('.product-card').hover(), $('.product-card').hover(
         function() {
-            $( this ).find('product-card__visual').addClass( "product-card__visual_hover" );
+            $( this ).find('.product-card__visual').addClass( "product-card__visual_hover" );
         }, function() {
-            $( this ).find('product-card__visual').removeClass( "product-card__visual_hover" );
+            $( this ).find('.product-card__visual').removeClass( "product-card__visual_hover" );
         }
-    );
+    ));
 });
--- src/project.blocks/common.blocks/product-card/product-card.js
+++ src/project.blocks/common.blocks/product-card/product-card.js
@@ -2,9 +2,11 @@
 
 /// Добавление класса
 $(function() {
-    $(".product-card").hover(function (){
-            $(this).addClass('product-card_hover');
-        }, function (){
-            $(this).removeClass('product-card_hover');
-    });
+    $.extend(true, $('.product-card').hover(), $('.product-card').hover(
+        function() {
+            $( this ).addClass('product-card_hover');
+        }, function() {
+            $( this ).removeClass('product-card_hover');
+        }
+    ));
 });
