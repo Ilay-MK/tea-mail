cd3cc924ea7dd7d62617ac6856896b751e0c9031
Ilya Purouski <MiKrob09@gmail.com>
2016-12-29 05:17:17 +0300

    Добавлен скрипт добавления класса "breadcrumb-arrow__item-content_last-child-item" последнему элементу "крошек"

--- src/project.blocks/common.blocks/breadcrumb-arrow/__item-content/breadcrumb-arrow__item-content.js
+++ src/project.blocks/common.blocks/breadcrumb-arrow/__item-content/breadcrumb-arrow__item-content.js
@@ -1,6 +1,9 @@
 "use strict";
 
 $(function() {
-    /* Добавление класса мода содержимого элемента хлебных крошек типа стрелки   */
+    /* Добавление класса мода содержимого элемента хлебных крошек типа стрелки кроме первого элемента крошек */
     $('.breadcrumb-arrow__item').not(':first').find('.breadcrumb-arrow__item-content').addClass('breadcrumb-arrow__item-content_not-first-child-item');
+
+    /* Добавление класса мода содержимого элемента хлебных крошек типа стрелки последнему элементу крошек */
+    $('.breadcrumb-arrow__item:last-child .breadcrumb-arrow__item-content').addClass('breadcrumb-arrow__item-content_last-child-item');
 });
