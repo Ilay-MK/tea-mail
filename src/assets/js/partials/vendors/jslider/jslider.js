0a85973b9cbbfe777af023f76ce7e3ff8dc3df4f
Ilya Purouski <MiKrob09@gmail.com>
2016-12-30 20:33:28 +0300

    Изменён объект инициализации слайдера диапозон цен в соответствии с новой разметкой

--- src/assets/js/partials/vendors/jslider/jslider.js
+++ src/assets/js/partials/vendors/jslider/jslider.js
@@ -1,9 +1,14 @@
-jQuery("#slider-price").slider({
-    from: 0,
-    to: 1000,
-    step: 1,
-    heterogeneity: ['50/100', '75/250'],
-    limits: true,
-    dimension: '&nbsp; руб.',
-    skin: "round"
+"use strict";
+
+$(function() {
+
+    jQuery("#catalog-filter__slider-price").slider({
+        from: 0,
+        to: 1000,
+        step: 1,
+        heterogeneity: ['50/100', '75/250'],
+        limits: true,
+        dimension: '&nbsp; руб.',
+        skin: "round"
+    });
 });
