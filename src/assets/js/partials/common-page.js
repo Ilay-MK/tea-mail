0c159473a0fc562c4d2b2bac7b57b68af7048e52
Ilya Purouski <MiKrob09@gmail.com>
2017-01-02 19:13:16 +0300

    Добавлена Ajax функция send form и перехват submit form

--- src/assets/js/partials/common-page.js
+++ src/assets/js/partials/common-page.js
@@ -124,4 +124,67 @@ $( document ).ready(function() {
         }
     ));
 
+    /* ------------ */
+    /* -= SEND FORM =- */
+    // Ajax send mail
+    $("form").submit(function (e) {
+
+        if ( $( this ).hasClass('form_catalog-filter') ) {
+            ajax(this, "./catalog-filter.php");
+        }
+
+        return false;
+	});
+
 });
+
+/// TEMP
+function ajax(ob, processor) {
+    var msg;
+    var result;
+
+    var result = $("#result");
+
+    if (processor === undefined) {
+        processor = "./mail.php";
+    }
+
+    $.ajax({
+        type: "POST",
+        url: processor,
+        data: $(ob).serialize(),
+        beforeSend: function(data) { // сoбытиe дo oтпрaвки
+            /*$(ob).find('input[type="submit"]').attr('disabled', 'disabled');*/ // нaпримeр, oтключим кнoпку, чтoбы нe жaли пo 100 рaз
+            //$(ob).find('.submit').attr('disabled', 'disabled');
+        },
+        complete: function(data) { // сoбытиe пoслe любoгo исхoдa
+            /*$(ob).find('input[type="submit"]').prop('disabled', false);*/ // в любoм случae включим кнoпку oбрaтнo
+            //$(ob).find('.submit').prop('disabled', false);
+        },
+        error: function (xhr, ajaxOptions, thrownError) {
+            /*result.addClass("text-danger bg-danger").text("Пожалуйста, проверьте введённые данные!");*/
+            /*alert("Ошибка: "+xhr.responseCode+" | сообщение: "+str);*/ /* отладка */
+            //alert("send email ERROR! "+ xhr.responseText); /* xhr.responseCode */
+            //alert(xhr.status); // пoкaжeм oтвeт сeрвeрa
+            //alert(thrownError); // и тeкст oшибки
+        },
+        success: function(data) {
+            //alert(data); /* $('.results').html(data); */
+        },
+    }).done(function (msg) {
+
+        /*if(msg === "OK"){
+            var result = "<div = 'bg-success'>Спасибо за заявку! Мы вам перезвоним!</div>"
+            form.html(result);
+        } else {
+            form.html(msg);
+        }*/
+
+        //$(ob).find("[type='text']").val("");
+        //$(ob).trigger("reset");
+
+
+    });
+
+    return false;
+}
