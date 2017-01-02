431c3bfa181c6bcfc4a264018a5bebd6f3ae48bb
Ilya Purouski <MiKrob09@gmail.com>
2017-01-02 16:44:58 +0300

    Добавлены скрипты catalog-filter

---
+++ src/project.blocks/catalog-page.blocks/catalog-filter/__slider-price/catalog-filter__slider-price.js
@@ -0,0 +1,16 @@
+"use strict";
+
+$(function() {
+    /*$('#catalog-filter__slider-price').slider("value");*/
+    /*$('#catalog-filter__slider-price').slider("value", p1, p2);*/
+    /*$('#catalog-filter__slider-price').slider("prc");*/
+    /*$('#catalog-filter__slider-price').slider("prc", p1, p2);*/
+    /*$('#catalog-filter__slider-price').slider("calculatedValue");*/
+
+    var val, p1, p2;
+
+    val = $('#catalog-filter__slider-price').slider("value");
+
+    alert(val);
+
+});
--- src/project.blocks/catalog-page.blocks/index.js
+++ src/project.blocks/catalog-page.blocks/index.js
@@ -1,3 +1,4 @@
+//= catalog-filter/index.js
 //= menu-catalog/index.js
 //= menu-main/index.js
 //= product-card/index.js
