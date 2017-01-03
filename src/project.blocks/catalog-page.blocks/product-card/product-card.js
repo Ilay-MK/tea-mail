83a9885280db060cdb6a30636bc52033e17fa3fc
Ilya Purouski <MiKrob09@gmail.com>
2017-01-03 07:16:31 +0300

    Добавлены стили и скрипт для анимации появления шевронов в карусели карточки продукта в catalog-page слое

--- src/project.blocks/catalog-page.blocks/product-card/product-card.js
+++ src/project.blocks/catalog-page.blocks/product-card/product-card.js
@@ -1,5 +1,12 @@
 "use strict";
 
+/// Добавление класса
 $(function() {
-    // Handler for .ready() called.
+    $.extend(true, $('.product-card').hover(), $('.product-card').hover(
+        function() {
+            $( this ).find('.catalog-carousel__control').addClass('catalog-carousel__control_hover');
+        }, function() {
+            $( this ).find('.catalog-carousel__control').removeClass('catalog-carousel__control_hover');
+        }
+    ));
 });
