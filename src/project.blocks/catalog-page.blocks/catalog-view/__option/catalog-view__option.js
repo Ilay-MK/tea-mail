7805644f43330abea1a6bcf246bc661b1e8e701a
Ilya Purouski <MiKrob09@gmail.com>
2017-01-04 02:47:16 +0300

    Добавлен элемент description блока product-card с модами отображения и добавлен стиль мода  _view_block. Добавлен скрипт добавления класса мода

--- src/project.blocks/catalog-page.blocks/catalog-view/__option/catalog-view__option.js
+++ src/project.blocks/catalog-page.blocks/catalog-view/__option/catalog-view__option.js
@@ -12,12 +12,16 @@ $(function() {
                 $('.product-card').addClass('product-card_view_block');
                 /* работаем с блоком visual */
                 $('.product-card__visual').addClass('product-card__visual_view_block');
+                /* работаем с контейнером описания продукта */
+                $('.product-card__description').addClass('product-card__description_view_block');
             }
             else {
                 /* работаем непосредсвенно с блоком-карточкой */
                 $('.product-card').removeClass('product-card_view_block');
                 /* работаем с блоком visual */
                 $('.product-card__visual').removeClass('product-card__visual_view_block');
+                /* работаем с контейнером описания продукта */
+                $('.product-card__description').removeClass('product-card__description_view_block');
             }
         }
     ));
