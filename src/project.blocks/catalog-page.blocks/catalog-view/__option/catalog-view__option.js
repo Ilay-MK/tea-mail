f2dd79a54208179124835546e2b012ca504f5e8c
Ilya Purouski <MiKrob09@gmail.com>
2017-01-04 02:28:05 +0300

    Добавлен скрипт добавления класса мода отображения на всю строку для блока visual карточки продукта

--- src/project.blocks/catalog-page.blocks/catalog-view/__option/catalog-view__option.js
+++ src/project.blocks/catalog-page.blocks/catalog-view/__option/catalog-view__option.js
@@ -8,10 +8,16 @@ $(function() {
 
             /* Добавляем мод карточки продукта для отображения на всю строчку */
             if($( this ).hasClass("catalog-view__option_block")) {
+                /* работаем непосредсвенно с блоком-карточкой */
                 $('.product-card').addClass('product-card_view_block');
+                /* работаем с блоком visual */
+                $('.product-card__visual').addClass('product-card__visual_view_block');
             }
             else {
+                /* работаем непосредсвенно с блоком-карточкой */
                 $('.product-card').removeClass('product-card_view_block');
+                /* работаем с блоком visual */
+                $('.product-card__visual').removeClass('product-card__visual_view_block');
             }
         }
     ));
