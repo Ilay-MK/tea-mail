40c0aac213a5f31471a4bee09b364cd7a084fab3
Ilya Purouski <MiKrob09@gmail.com>
2017-01-04 02:14:44 +0300

    Добавлен мод product-card -  _view_block и скрипт добавление этого класса при выборе соответствующего отображения товаров в опциях отображения\

--- src/project.blocks/catalog-page.blocks/catalog-view/__option/catalog-view__option.js
+++ src/project.blocks/catalog-page.blocks/catalog-view/__option/catalog-view__option.js
@@ -5,6 +5,14 @@ $(function() {
     $.extend(true, $('.catalog-view__option').click(), $('.catalog-view__option').click(
         function () {
             $(this).stop().animateCss("bounceIn");
+
+            /* Добавляем мод карточки продукта для отображения на всю строчку */
+            if($( this ).hasClass("catalog-view__option_block")) {
+                $('.product-card').addClass('product-card_view_block');
+            }
+            else {
+                $('.product-card').removeClass('product-card_view_block');
+            }
         }
     ));
 });
