475257c9c4bbeb9f98494c58739f244c883de919
Ilya Purouski <MiKrob09@gmail.com>
2017-01-04 03:08:40 +0300

    Добавлен скрипт добавления класса мода для контейнера кнокпи отображения в строку продукта

--- src/project.blocks/catalog-page.blocks/catalog-view/__option/catalog-view__option.js
+++ src/project.blocks/catalog-page.blocks/catalog-view/__option/catalog-view__option.js
@@ -14,6 +14,8 @@ $(function() {
                 $('.product-card__visual').addClass('product-card__visual_view_block');
                 /* работаем с контейнером описания продукта */
                 $('.product-card__description').addClass('product-card__description_view_block');
+                /* работаем с контейнером кнопки продукта */
+                $('.product-card__btn-container').addClass('product-card__btn-container_view_block');
             }
             else {
                 /* работаем непосредсвенно с блоком-карточкой */
@@ -22,6 +24,8 @@ $(function() {
                 $('.product-card__visual').removeClass('product-card__visual_view_block');
                 /* работаем с контейнером описания продукта */
                 $('.product-card__description').removeClass('product-card__description_view_block');
+                /* работаем с контейнером кнопки продукта */
+                $('.product-card__btn-container').removeClass('product-card__btn-container_view_block');
             }
         }
     ));
