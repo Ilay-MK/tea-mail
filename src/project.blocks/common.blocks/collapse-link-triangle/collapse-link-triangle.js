bd232f68c6bc2572a8e18e48c6e5b6f223757c69
Ilya Purouski <MiKrob09@gmail.com>
2017-01-05 06:43:45 +0300

    Модифицирован скрипт добавления класса мода active collapse-link-triangle

--- src/project.blocks/common.blocks/collapse-link-triangle/collapse-link-triangle.js
+++ src/project.blocks/common.blocks/collapse-link-triangle/collapse-link-triangle.js
@@ -2,7 +2,7 @@
 
 $(function() {
     /// Расширение функции
-    $.extend(true, $('.collapse-link-triangle').click(), $('.collapse-link-triangle').click( function (e) {
+    /*$.extend(true, $('.collapse-link-triangle').click(), $('.collapse-link-triangle').click( function (e) {
         e.preventDefault();
         var obj;
 
@@ -14,6 +14,20 @@ $(function() {
         else {
             $( this ).removeClass('collapse-link-triangle_active');
         }
-    }));
+    }));*/
+
+    $('.collapse-link-triangle').click( function (e) {
+        e.preventDefault();
+        var obj;
+
+        obj = $( this ).attr("data-target");
+
+        if($(obj).is(':hidden')) {
+            $( this ).addClass('collapse-link-triangle_active');
+        }
+        else {
+            $( this ).removeClass('collapse-link-triangle_active');
+        }
+    });
 
 });
