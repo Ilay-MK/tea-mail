437b9334ef233804db48f4661448cba2d9c00d3e
Ilya Purouski <MiKrob09@gmail.com>
2017-01-05 04:40:37 +0300

    Добавлен скрипт колапса меню каталога с изменением лэйбла

--- src/project.blocks/common.blocks/page/__link-toggle/page__link-toggle.js
+++ src/project.blocks/common.blocks/page/__link-toggle/page__link-toggle.js
@@ -17,4 +17,34 @@ $(function() {
         });
 
     });
+
+    /// Расширение функции
+    /*$.extend(true, $('.page__link-toggle_target_menu-catalog').click(), $('.page__link-toggle_target_menu-catalog').click(function (e) {
+        var obj;
+
+        obj = $( this ).attr("data-target");
+
+        if($(obj).is(':hidden')) {
+            $( this ).text("Показать каталог");
+        }
+        else {
+            $( this ).text("Скрыть каталог");
+        }
+    }));*/
+
+    /// Событие click() с отменой перехода по ссылке
+    $(".page__link-toggle_target_menu-catalog").click(function (e) {
+        e.preventDefault();
+        var obj;
+
+        obj = $( this ).attr("data-target");
+
+        /* инверсно, т.к. видимо сперва проверка а затем колапс */
+        if($(obj).is(':hidden')) {
+            $( this ).text("Скрыть каталог");
+        }
+        else {
+            $( this ).text("Показать каталог");
+        }
+    });
 });
