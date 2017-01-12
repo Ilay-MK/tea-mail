2ae3018565d82b0885f16dbcf17271eb7f533902
Ilya Purouski <MiKrob09@gmail.com>
2017-01-12 10:56:56 +0300

    Добавление класса "chevron__inner_hover" срабатывает не на "catalog-carousel__control" а на "carousel-control"

--- src/project.blocks/common.blocks/chevron/__inner/chevron__inner.js
+++ src/project.blocks/common.blocks/chevron/__inner/chevron__inner.js
@@ -2,7 +2,7 @@
 
 /// Добавление класса .chevron__inner_hover при hover контрола
 $(function() {
-    $.extend(true, $('.catalog-carousel__control').hover(), $('.catalog-carousel__control').hover(
+    $.extend(true, $('.carousel-control').hover(), $('.carousel-control').hover(
         function() {
             $( this ).find('.chevron__inner').addClass( "chevron__inner_hover" );
         }, function() {
