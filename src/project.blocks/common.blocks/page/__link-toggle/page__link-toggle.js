e12150539243ba83a94538834dcff35f2d291d39
Ilya Purouski <MiKrob09@gmail.com>
2017-01-18 11:34:01 +0300

    Убран ненужный скрипт в page__link-toggle

--- src/project.blocks/common.blocks/page/__link-toggle/page__link-toggle.js
+++ src/project.blocks/common.blocks/page/__link-toggle/page__link-toggle.js
@@ -2,33 +2,4 @@
 
 $(function() {
 
-    /// Расширение функции
-    /*$.extend(true, $('.page__link-toggle_target_menu-catalog').click(), $('.page__link-toggle_target_menu-catalog').click(function (e) {
-        var obj;
-
-        obj = $( this ).attr("data-target");
-
-        if($(obj).is(':hidden')) {
-            $( this ).text("Показать каталог");
-        }
-        else {
-            $( this ).text("Скрыть каталог");
-        }
-    }));*/
-
-    /// Событие click() с отменой перехода по ссылке
-    $(".page__link-toggle_target_menu-catalog").click(function (e) {
-        e.preventDefault();
-        var obj;
-
-        obj = $( this ).attr("data-target");
-
-        /* инверсно, т.к. колапс занимает некоторое время */
-        if($(obj).is(':hidden')) {
-            $( this ).text("Скрыть каталог");
-        }
-        else {
-            $( this ).text("Показать каталог");
-        }
-    });
 });
