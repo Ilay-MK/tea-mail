6d3516a8190a1898e6ef3589ecede5a151a7dca7
Ilya Purouski <MiKrob09@gmail.com>
2017-01-18 10:47:17 +0300

    Переписан скрипт подмены текста линка-toggle collapse'а блока text-collapsing

--- src/project.blocks/common.blocks/text-collapsing/__link-toggle/text-collapsing__link-toggle.js
+++ src/project.blocks/common.blocks/text-collapsing/__link-toggle/text-collapsing__link-toggle.js
@@ -6,17 +6,35 @@ $(function() {
     /// Toggle описание страницы.
     $(".text-collapsing__link-toggle").click(function (e) {
         e.preventDefault();
-        var linkToggle, obj;
+        if ( $( this ).attr("data-alttext") === undefined ) return;
+
+        /* --- */
+
+        var linkToggle, obj, text, altText;
+
+        /* --- */
 
         linkToggle  = $( this );
-        obj         = linkToggle.attr("data-target");
 
+        /*obj         = linkToggle.attr("data-target");*/
+        altText     = linkToggle.attr("data-alttext");
+        text        = linkToggle.text();
+
+        /* --- */
+
+        linkToggle.attr("data-alttext", text);
+        linkToggle.text(altText);
+
+        /* --- */
+
+        /*
         if($(obj).attr('aria-expanded') == 'false') {
-            linkToggle.text("Свернуть описание");
+
         }
         else {
-            linkToggle.text("Читать полностью");
+
         }
+        */
 
     });
 
