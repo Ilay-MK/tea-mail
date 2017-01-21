9eb20a9df36402734da02342072dcbdfa74be34e
Ilya Purouski <MiKrob09@gmail.com>
2017-01-21 20:49:14 +0300

    Реализовано меньше 768px оыбычная simple item carousel bt3 в похожих товаров

--- src/project.blocks/common.blocks/similar-product/__carousel-control/similar-product_carousel-control.js
+++ src/project.blocks/common.blocks/similar-product/__carousel-control/similar-product_carousel-control.js
@@ -7,11 +7,15 @@ $(function() {
     });
 
     $('.similar-product .carousel-control.right').click(function() {
-        $(this).blur();
-        $(this).parent().find('.item').first().insertAfter($(this).parent().find('.item').last());
+        if (getPageSize()[2] > 767) {
+            $(this).blur();
+            $(this).parent().find('.item').first().insertAfter($(this).parent().find('.item').last());
+        }
     });
     $('.similar-product .carousel-control.left').click(function() {
-        $(this).blur();
-        $(this).parent().find('.item').last().insertBefore($(this).parent().find('.item').first());
+        if (getPageSize()[2] > 767) {
+            $(this).blur();
+            $(this).parent().find('.item').last().insertBefore($(this).parent().find('.item').first());
+        }
     });
 });
