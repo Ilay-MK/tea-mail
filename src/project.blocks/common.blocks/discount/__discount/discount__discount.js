1c1d2484e908d7f8a3abd9ea4ff9acf38f856ce1
Ilya Purouski <MiKrob09@gmail.com>
2017-01-24 19:54:36 +0300

    Добавлен мод hover элемента discount блока discount со стилем  и сриптом добавления класса-мода при hover discount__discount. Отредактированы отступы блока

--- src/project.blocks/common.blocks/discount/__discount/discount__discount.js
+++ src/project.blocks/common.blocks/discount/__discount/discount__discount.js
@@ -5,8 +5,10 @@ $(function() {
     $.extend(true, $('.discount__discount').hover(), $('.discount__discount').hover(
         function() {
             $( this ).find('.discount__img-card').addClass('discount__img-card_hover');
+            $( this ).addClass('discount__discount_hover');
         }, function() {
             $( this ).find('.discount__img-card').removeClass('discount__img-card_hover');
+            $( this ).removeClass('discount__discount_hover');
         }
     ));
 });
