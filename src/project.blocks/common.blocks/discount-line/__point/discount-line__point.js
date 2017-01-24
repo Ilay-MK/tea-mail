2165e4a973b9cc3b0e21b92d86b89cffeae8bb13
Ilya Purouski <MiKrob09@gmail.com>
2017-01-24 13:29:57 +0300

    discount-line__point - добавлен скрипт отступов ежду точками динамический

--- src/project.blocks/common.blocks/discount-line/__point/discount-line__point.js
+++ src/project.blocks/common.blocks/discount-line/__point/discount-line__point.js
@@ -1,5 +1,28 @@
 "use strict";
 
-$(function() {
+/// Позиционирование Точек на линии дисконта.
+function positioningDiscountLinePoint() {
+    var countVisiblePoints,
+        $pointVisible,
+        rate = 2;
 
+    $('#discount .discount-line__point').css("margin-left", "0").css("margin-right", "0");
+
+    $pointVisible = $('#discount .discount-line__point:visible');
+    countVisiblePoints = +$pointVisible.length;
+    rate += rate * (countVisiblePoints > 2 ? countVisiblePoints-rate : 0) ;
+
+    $pointVisible.not($pointVisible[0])
+        .css("margin-left", ( 100/rate ) + "%");
+    $pointVisible.not(":last-child")
+        .css("margin-right", ( 100/rate ) + "%");
+}
+
+$(function() {
+    positioningDiscountLinePoint();
+
+    /// Расширение функции
+    $.extend(true, $(window).resize(), $(window).resize(function() {
+        positioningDiscountLinePoint();
+    }));
 });
