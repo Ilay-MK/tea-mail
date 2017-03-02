0b4bd3611eba33be8426d58ce003ebb0bbe0dd95
Ilya Purouski <MiKrob09@gmail.com>
2017-03-02 21:55:05 +0300

    Реализовано "листание" шагов оформления заказа в корзине

--- src/project.blocks/cart-page.blocks/cart/cart.js
+++ src/project.blocks/cart-page.blocks/cart/cart.js
@@ -7,7 +7,25 @@ function currentStep() {
 
 /// Отображение атрибутов соответствующего шага и контролов.
 function displayStepAttributes() {
-    var purposeAttr = "data-purpose__text_" + currentStep();
+    var purposeAttr = "data-purpose__text_" + currentStep(),
+        $currStep;
+
+    if (currentStep())
+
+    switch( currentStep() ) {
+        case 'delivery':
+            $currStep = $( ".cart__delivery" );
+        break;
+
+        case 'payment':
+            $currStep = $( ".cart__payment" );
+        break;
+
+        default:
+            $currStep = $( ".cart__purchases" );
+    }
+
+    $currStep.show();
 
     $('.cart__steps-ctrl[' + purposeAttr + ']').each(function() {
         $( this ).addClass('cart__steps-ctrl_displayed').find('.label').text( $( this ).attr(purposeAttr) );
@@ -20,6 +38,9 @@ $(function() {
     $('.cart-steps__step').bind('cssClassChanged', function() {
         // Удаляем класс обображения контролов
         $('.cart__steps-ctrl').removeClass('cart__steps-ctrl_displayed');
+
+        // Прячем шаги
+        $('.cart__step-container').hide();
 
         // Отработает, как только сменится класс
         displayStepAttributes();
