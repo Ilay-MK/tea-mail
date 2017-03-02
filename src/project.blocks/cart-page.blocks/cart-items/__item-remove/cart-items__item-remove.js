23ee68eab5598ff505f0ad9fa9fabd5d269e0c13
Ilya Purouski <MiKrob09@gmail.com>
2017-03-02 09:05:36 +0300

    Добавлен скрипт удаление товара из покупаемого списка (корхины).

--- src/project.blocks/cart-page.blocks/cart-items/__item-remove/cart-items__item-remove.js
+++ src/project.blocks/cart-page.blocks/cart-items/__item-remove/cart-items__item-remove.js
@@ -2,6 +2,9 @@
 
 $(function() {
 
-
+    /// Удаление товара из покупаемого списка (корхины).
+    $('.cart-items__item-remove').click(function (e) {
+        $( this ).closest('.product-card').remove();
+    });
 
 });
