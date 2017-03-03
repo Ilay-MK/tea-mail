c8efbf5f1cc7038fb642bbb8389e1485126b4781
Ilya Purouski <MiKrob09@gmail.com>
2017-03-03 14:00:15 +0300

    delivery__btn-scope - скрипт активации табов инфы по доставке или оплате

--- src/project.blocks/delivery-page.blocks/delivery/__btn-scope/delivery__btn-scope.js
+++ src/project.blocks/delivery-page.blocks/delivery/__btn-scope/delivery__btn-scope.js
@@ -6,8 +6,13 @@ $(function() {
     $('.delivery__btn-scope').click(function () {
         if( $( this ).hasClass('delivery__btn-scope_active') ) return;
 
+        var activeScope = $( this ).attr('data-target');
+
         $('.delivery__btn-scope').removeClass('delivery__btn-scope_active');
+        $('.delivery__scope').removeClass('delivery__scope_active');
+
         $( this ).addClass('delivery__btn-scope_active');
+        $( activeScope ).addClass('delivery__scope_active');
     });
 
 });
