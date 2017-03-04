cebd9b7dee1d535f6ba08649e9c090efb77579e1
Ilya Purouski <MiKrob09@gmail.com>
2017-03-04 17:38:10 +0300

    Модифицирована функция detachPrependTo

--- src/project.blocks/common.blocks/detachPrependTo/detachPrependTo.js
+++ src/project.blocks/common.blocks/detachPrependTo/detachPrependTo.js
@@ -6,15 +6,29 @@
 /// @from - откуда производится перенос
 /// @to - куда производится перенос
 /// @width - при какой ширине производить перенос { < width }
-function detachPrependTo(obj, from, to, width, insertAfter) {
+function detachPrependTo(obj, from, to, width, insertAfter, inverse) {
     if (width === undefined) {
         width = 768;
     }
     if (insertAfter === undefined) {
         insertAfter = false;
     }
+    if (inverse === undefined) {
+        inverse = false;
+    }
 
-    if (getPageSize()[2] < width) {
+    var condition;
+
+    if( inverse ) {
+        condition = getPageSize()[2] > width-1;
+        console.log(condition);
+    }
+    else {
+        condition = getPageSize()[2] < width;
+        console.log(condition);
+    }
+
+    if (condition) {
         /* перемещаем контейнер */
         if (insertAfter) {
             $(from).find(obj).detach().insertAfter(to);
